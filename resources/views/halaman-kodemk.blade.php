08f1e6926af0d7fb0c7ed764d0afe482aae18bcb
joycelynpcr <joycelyn24si@mahasiswa.pcr.ac.id>
2025-09-26 08:03:40 +0700

    versi awal

---
+++ resources/views/halaman-kodemk.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Selamat Datang!</h1>
+        <h2>Masukkan kode matakuliah</h2>
+    </body>
+</html>
---
+++ resources/views/halaman-matakuliah.show.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Selamat Datang!</h1>
+        <h2>Anda mengakses matakuliah ST445</h2>
+    </body>
+</html>
